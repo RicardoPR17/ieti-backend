bd018b39fdb77f2cc831e5595191ea7c090fcc36
RicardoPR17 <rpulidor17.rpr18@gmail.com>
2024-02-24 20:45:44 -0500

    Documentos base y ordenes funcionando

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const userRouter = require("./routes/users");
+const ordersRouter = require("./routes/orders");
+const providersRouter = require("./routes/providers");
+
+dotenv.config();
+const app = express();
+app.disable("x-powered-by");
+
+//Middleware
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cors());
+// app.use("/", userRouter.router);
+app.use("/", ordersRouter.router);
+// app.use("/", providersRouter.router);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`A NodeJS API is listening on port ${port}`);
+});
