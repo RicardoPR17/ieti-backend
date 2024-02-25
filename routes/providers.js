7cb7870aa03bb1ac83455bbf06ebd2f2a2bc9220
JessicaDMunozO <jessitae@gmail.com>
2024-02-25 12:29:56 -0500

    Corrección estilo

--- routes/providers.js
+++ routes/providers.js
@@ -5,14 +5,11 @@ const router = express.Router();
 // Rutas GET:
 router.get("/db/providers", providers.getAllProviders);
 
-// Rutas GET:
 router.get("/db/providers/:namePharmacy", providers.getProvider);
 
-// Rutas GET:
 router.get("/db/providers/medicine/:medicine", providers.getMedicineProvider);
 
 // Rutas POST:
 router.post("/db/providers/new", providers.createProvider);
-
 
 module.exports = { router };
